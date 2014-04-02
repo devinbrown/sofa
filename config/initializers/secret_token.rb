de1022a26b11fbc94ca1519ee6b1343f080e4f3a
Devin Brown <devin.g.brown@gmail.com>
2014-04-02 17:00:29 -0600

    Initial local commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sofa::Application.config.secret_key_base = '3a09f9bf2cd25c1dd3da8c10a5a240ade60338c55d53ff4567fec5b59912beeda7a549286308d5a9fdb956e4d5dc7acff12f9fbd3c43c1f87fb067a7e99c6731'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sofa::Application.config.session_store :cookie_store, key: '_sofa_session'
